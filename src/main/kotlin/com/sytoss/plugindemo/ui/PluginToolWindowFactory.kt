16e697a70b1ea6bb51058e38c9084fb128a6d786
Vladyslav Koshel <koshel.vladyslav@gmail.com>
2023-07-24 11:40:09 +0200

    moving PluginToolWindowFactory to the ui directory

--- src/main/kotlin/com/sytoss/plugindemo/ui/PluginToolWindowFactory.kt
+++ src/main/kotlin/com/sytoss/plugindemo/ui/PluginToolWindowFactory.kt
@@ -1,11 +1,10 @@
-package com.sytoss.plugindemo
+package com.sytoss.plugindemo.ui
 
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
-import com.sytoss.plugindemo.ui.PluginToolWindowContent
 
 class PluginToolWindowFactory : ToolWindowFactory, DumbAware {
 
