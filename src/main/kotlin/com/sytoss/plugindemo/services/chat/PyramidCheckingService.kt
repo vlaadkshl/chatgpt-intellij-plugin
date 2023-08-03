5826cc2e6fa726e67637ea3696ac54ac9ae56c62
Vladyslav Koshel <koshel.vladyslav@gmail.com>
2023-08-03 17:56:27 +0200

    - creating systemMessage

--- src/main/kotlin/com/sytoss/plugindemo/services/chat/PyramidCheckingService.kt
+++ src/main/kotlin/com/sytoss/plugindemo/services/chat/PyramidCheckingService.kt
@@ -22,6 +22,42 @@ object PyramidCheckingService : ChatAnalysisAbstractService() {
             throw IllegalStateException("No pyramid was found!")
         }
 
+        val systemMessage = """
+            |You are a helpful assistant.
+            |You check classes for compliance with given conditions
+            |Conditions are instructions of what should or should not be in these classes
+            |User gives you conditions in JSON format like this:
+            |{
+            |   "converter": [{
+            |       "name": "{some class name}",
+            |       "add": [
+            |           "{some field or method}",
+            |           "{some field or method}"
+            |       ],
+            |       "remove": [
+            |           "{some field or method}"
+            |       ]
+            |   }],
+            |   "bom": [{the same as in converter}],
+            |   "dto": [{the same as in converter}],
+            |   "interface": [{the same as in converter}],
+            |   "service": [{the same as in converter}],
+            |   "connector": [{the same as in converter}]
+            |}
+            |
+            |Show comments in JSON format like this:
+            |{
+            |    "result": [
+            |        "className": "package.ClassName",
+            |        "warnings": [{
+            |            "warning": "{Place Warning here}",
+            |            "lineInCode": "{place line with error here}"
+            |        }]
+            |    ]
+            |}
+            |If there is no errors for class, don't put it in result
+        """.trimMargin()
+
         return mutableListOf()
     }
 }
