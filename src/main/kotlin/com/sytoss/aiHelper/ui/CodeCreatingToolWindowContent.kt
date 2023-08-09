1ccf1ee25aefb3849f0dba35ad40003e545c0cf2
Vladyslav Koshel <koshel.vladyslav@gmail.com>
2023-08-09 15:21:24 +0200

    - added empty panel into ScrollWithInsets onto Creating Code Toolwindow

--- src/main/kotlin/com/sytoss/aiHelper/ui/CodeCreatingToolWindowContent.kt
+++ src/main/kotlin/com/sytoss/aiHelper/ui/CodeCreatingToolWindowContent.kt
@@ -1,9 +1,21 @@
 package com.sytoss.aiHelper.ui
 
 import com.intellij.openapi.project.Project
+import com.sytoss.aiHelper.ui.components.ScrollWithInsets
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
 import javax.swing.JPanel
 
 class CodeCreatingToolWindowContent(private val project: Project) {
 
-    val contentPanel = JPanel()
+    private val mainPanel = JPanel(GridBagLayout())
+
+    val contentPanel = ScrollWithInsets { mainPanel }
+
+    private val constraints = GridBagConstraints()
+
+    init {
+        constraints.gridx = 0
+        constraints.anchor = GridBagConstraints.PAGE_START
+    }
 }
