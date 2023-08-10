3af01cce169cf1cf5bf633b807085b2bb4297a9f
Vladyslav Koshel <koshel.vladyslav@gmail.com>
2023-08-10 13:42:10 +0200

    - created split panel in Create Code toolwindow

--- src/main/kotlin/com/sytoss/aiHelper/ui/CodeCreatingToolWindowContent.kt
+++ src/main/kotlin/com/sytoss/aiHelper/ui/CodeCreatingToolWindowContent.kt
@@ -1,6 +1,7 @@
 package com.sytoss.aiHelper.ui
 
 import com.intellij.openapi.project.Project
+import com.intellij.ui.OnePixelSplitter
 import com.sytoss.aiHelper.services.PumlDiagramChooser
 import com.sytoss.aiHelper.ui.components.DefaultConstraints
 import com.sytoss.aiHelper.ui.components.FileChooserCreateComponent
@@ -15,17 +16,20 @@ class CodeCreatingToolWindowContent(private val project: Project) {
 
     private val mainPanel = JPanel(GridBagLayout())
 
-    val contentPanel = ScrollWithInsets { mainPanel }
+    val contentPanel = OnePixelSplitter()
 
     private val pumlChooserBtn = JButtonWithListener("Choose PlantUML file") {
         PumlDiagramChooser.selectFile(it.source as JButton, project)
     }
 
-    private val bomChooser = FileChooserCreateComponent("Choose BOM Files", project)
+    private val bomChooser = FileChooserCreateComponent("Create DTO based on BOM", project)
 
     init {
+        contentPanel.firstComponent = ScrollWithInsets { mainPanel }
         addWithConstraints(pumlChooserBtn)
         addWithConstraints(bomChooser)
+
+        contentPanel.secondComponent = ScrollWithInsets { JPanel() }
     }
 
     private fun addWithConstraints(component: JComponent) {
