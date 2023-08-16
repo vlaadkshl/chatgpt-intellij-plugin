de5e79d0848eae20b820bab17dd45d1e407b7cb3
Vladyslav Koshel <koshel.vladyslav@gmail.com>
2023-08-16 12:37:50 +0200

    - extracted examples of BOM, DTO and Converter into different values

--- src/main/kotlin/com/sytoss/aiHelper/services/codeCreating/Creators.kt
+++ src/main/kotlin/com/sytoss/aiHelper/services/codeCreating/Creators.kt
@@ -56,7 +56,7 @@ object Creators {
         return result
     }
 
-    private fun createBom(pumlContent: String): CreateResponse? {
+    fun createBom(pumlContent: String): CreateResponse? {
         val request = CreateRequest(
             model = ModelType.GPT,
             prompt = """
@@ -65,59 +65,13 @@ object Creators {
                     PUML diagram:
                     $pumlContent
                 """.trimIndent(),
-            example = """
-                    Hotel.java:
-                    ```java
-                    import lombok.AllArgsConstructor;
-                    import lombok.Builder;
-                    import lombok.Data;
-                    import lombok.NoArgsConstructor;
-                    
-                    import java.util.List;
-                    
-                    @Data
-                    @Builder
-                    @NoArgsConstructor
-                    @AllArgsConstructor
-                    public class Hotel {
-                        private long id;
-                    
-                        private String address;
-                    
-                        private String type;
-                    
-                        private List<Room> rooms;
-                    }
-                    ```
-                    
-                    Room.java:
-                    ```java
-                    import lombok.AllArgsConstructor;
-                    import lombok.Builder;
-                    import lombok.Data;
-                    import lombok.NoArgsConstructor;
-                    
-                    @Data
-                    @Builder
-                    @NoArgsConstructor
-                    @AllArgsConstructor
-                    public class Room {
-                        private long id;
-                    
-                        private String number;
-                    
-                        private String level;
-                    
-                        private int capacity;
-                    }
-                    ```
-                """.trimIndent()
+            example = bomExample
         )
 
         return RequestSender.sendRequest(request)
     }
 
-    private fun createDto(pumlContent: String): CreateResponse? {
+    fun createDto(pumlContent: String): CreateResponse? {
         val request = CreateRequest(
             model = ModelType.GPT,
             prompt = """
@@ -126,134 +80,26 @@ object Creators {
                     PUML diagram:
                     $pumlContent
                 """.trimIndent(),
-            example = """
-                    CourseDto.java:
-                    ```java
-                    import jakarta.persistence.*;
-                    import lombok.AllArgsConstructor;
-                    import lombok.Builder;
-                    import lombok.Data;
-                    import lombok.NoArgsConstructor;
-                    
-                    import java.util.List;
-                    
-                    @Entity
-                    @Data
-                    @Builder
-                    @Table(name = "COURSE")
-                    @NoArgsConstructor
-                    @AllArgsConstructor
-                    public class CourseDto {
-                        @Id
-                        @GeneratedValue(strategy = GenerationType.IDENTITY)
-                        private long id;
-                    
-                        @Column(name = "name")
-                        private String name;
-                    }
-                    ```
-                    
-                    StudentDto.java:
-                    ```java
-                    import jakarta.persistence.*;
-                    import lombok.AllArgsConstructor;
-                    import lombok.Builder;
-                    import lombok.Data;
-                    import lombok.NoArgsConstructor;
-                    
-                    @Entity
-                    @Data
-                    @Builder
-                    @Table(name = "STUDENT")
-                    @NoArgsConstructor
-                    @AllArgsConstructor
-                    public class StudentDto {
-                        @Id
-                        @GeneratedValue(strategy = GenerationType.IDENTITY)
-                        private long id;
-                    
-                        @Column(name = "name")
-                        private String name;
-                    
-                        @ManyToOne
-                        @JoinColumn(name = "course_id")
-                        private CourseDto course;
-                    }
-                    ```
-                """.trimIndent()
+            example = dtoExample
         )
 
         return RequestSender.sendRequest(request)
     }
 
-    private fun createDto(bomElements: List<CreateResponse.CreateContent>): CreateResponse? {
+    fun createDto(bomElements: List<CreateResponse.CreateContent>): CreateResponse? {
         val request = CreateRequest(
             model = ModelType.GPT,
             prompt = """
                     Write java classes for DTO according to the BOM classes below:
                     ${bomElements.joinToString(separator = "\n") { it.body }}
                 """.trimIndent(),
-            example = """
-                    CourseDto.java:
-                    ```java
-                    import jakarta.persistence.*;
-                    import lombok.AllArgsConstructor;
-                    import lombok.Builder;
-                    import lombok.Data;
-                    import lombok.NoArgsConstructor;
-                    
-                    import java.util.List;
-                    
-                    @Entity
-                    @Data
-                    @Builder
-                    @Table(name = "COURSE")
-                    @NoArgsConstructor
-                    @AllArgsConstructor
-                    public class CourseDto {
-                        @Id
-                        @GeneratedValue(strategy = GenerationType.IDENTITY)
-                        private long id;
-                    
-                        @Column(name = "name")
-                        private String name;
-                    }
-                    ```
-                    
-                    StudentDto.java:
-                    ```java
-                    import jakarta.persistence.*;
-                    import lombok.AllArgsConstructor;
-                    import lombok.Builder;
-                    import lombok.Data;
-                    import lombok.NoArgsConstructor;
-                    
-                    @Entity
-                    @Data
-                    @Builder
-                    @Table(name = "STUDENT")
-                    @NoArgsConstructor
-                    @AllArgsConstructor
-                    public class StudentDto {
-                        @Id
-                        @GeneratedValue(strategy = GenerationType.IDENTITY)
-                        private long id;
-                    
-                        @Column(name = "name")
-                        private String name;
-                    
-                        @ManyToOne
-                        @JoinColumn(name = "course_id")
-                        private CourseDto course;
-                    }
-                    ```
-                """.trimIndent()
+            example = dtoExample
         )
 
         return RequestSender.sendRequest(request)
     }
 
-    private fun createConverters(
+    fun createConverters(
         boms: List<CreateResponse.CreateContent>,
         dtos: List<CreateResponse.CreateContent>
     ): CreateResponse? {
@@ -268,61 +114,167 @@ object Creators {
                     DTO:
                     ${dtos.joinToString(separator = "\n\n") { it.body }}
                 """.trimIndent(),
-            example = """
-                    BookConverter:
-                    ```java
-                    package com.sytoss.edu.library.converters;
-
-                    import com.sytoss.edu.library.bom.Author;
-                    import com.sytoss.edu.library.bom.Book;
-                    import com.sytoss.edu.library.dto.AuthorDTO;
-                    import com.sytoss.edu.library.dto.BookDTO;
-                    import com.sytoss.edu.library.dto.GenreDTO;
-                    import lombok.RequiredArgsConstructor;
-                    import org.springframework.stereotype.Component;
-
-                    import java.util.HashSet;
-                    import java.util.Set;
-                    import java.util.stream.Collectors;
-
-                    @Component
-                    @RequiredArgsConstructor
-                    public class BookConverter {
-
-                        private final AuthorConverter authorConverter;
-
-                        private final GenreConverter genreConverter;
-
-                        public void toDto(Book source, BookDTO destination) {
-                            destination.setName(source.getName());
-                            destination.setLanguage(source.getLanguage());
-                            destination.setYearOfPublishing(source.getYearOfPublishing());
-                            AuthorDTO authorDTO = new AuthorDTO();
-                            authorConverter.toDto(source.getAuthor(), authorDTO);
-                            destination.setAuthor(authorDTO);
-                            Set<GenreDTO> genres = new HashSet<>();
-                            genreConverter.toDto(source.getGenres(), genres);
-                            destination.setGenres(genres);
-                        }
-
-                        public void fromDTO(BookDTO source, Book book) {
-                            book.setId(source.getId());
-                            book.setName(source.getName());
-                            book.setLanguage(source.getLanguage());
-                            book.setYearOfPublishing(source.getYearOfPublishing());
-                            book.setGenres(source.getGenres()
-                                    .stream()
-                                    .map(genreConverter::fromDTO)
-                                    .collect(Collectors.toList()));
-                            Author author = new Author();
-                            authorConverter.fromDTO(source.getAuthor(), author);
-                            book.setAuthor(author);
-                        }
-                    }
-                    ```
-                """.trimIndent()
+            example = converterExample
         )
 
         return RequestSender.sendRequest(request)
     }
+
+    private val bomExample = """
+        Hotel.java:
+        ```java
+        import lombok.AllArgsConstructor;
+        import lombok.Builder;
+        import lombok.Data;
+        import lombok.NoArgsConstructor;
+        
+        import java.util.List;
+        
+        @Data
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public class Hotel {
+            private long id;
+        
+            private String address;
+        
+            private String type;
+        
+            private List<Room> rooms;
+        }
+        ```
+        
+        Room.java:
+        ```java
+        import lombok.AllArgsConstructor;
+        import lombok.Builder;
+        import lombok.Data;
+        import lombok.NoArgsConstructor;
+        
+        @Data
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public class Room {
+            private long id;
+        
+            private String number;
+        
+            private String level;
+        
+            private int capacity;
+        }
+        ```
+    """.trimIndent()
+
+    private val dtoExample = """
+        CourseDto.java:
+        ```java
+        import jakarta.persistence.*;
+        import lombok.AllArgsConstructor;
+        import lombok.Builder;
+        import lombok.Data;
+        import lombok.NoArgsConstructor;
+        
+        import java.util.List;
+        
+        @Entity
+        @Data
+        @Builder
+        @Table(name = "COURSE")
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public class CourseDto {
+            @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+            private long id;
+        
+            @Column(name = "name")
+            private String name;
+        }
+        ```
+        
+        StudentDto.java:
+        ```java
+        import jakarta.persistence.*;
+        import lombok.AllArgsConstructor;
+        import lombok.Builder;
+        import lombok.Data;
+        import lombok.NoArgsConstructor;
+        
+        @Entity
+        @Data
+        @Builder
+        @Table(name = "STUDENT")
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public class StudentDto {
+            @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+            private long id;
+        
+            @Column(name = "name")
+            private String name;
+        
+            @ManyToOne
+            @JoinColumn(name = "course_id")
+            private CourseDto course;
+        }
+        ```
+    """.trimIndent()
+
+    private val converterExample = """
+        BookConverter:
+        ```java
+        package com.sytoss.edu.library.converters;
+
+        import com.sytoss.edu.library.bom.Author;
+        import com.sytoss.edu.library.bom.Book;
+        import com.sytoss.edu.library.dto.AuthorDTO;
+        import com.sytoss.edu.library.dto.BookDTO;
+        import com.sytoss.edu.library.dto.GenreDTO;
+        import lombok.RequiredArgsConstructor;
+        import org.springframework.stereotype.Component;
+
+        import java.util.HashSet;
+        import java.util.Set;
+        import java.util.stream.Collectors;
+
+        @Component
+        @RequiredArgsConstructor
+        public class BookConverter {
+
+            private final AuthorConverter authorConverter;
+
+            private final GenreConverter genreConverter;
+
+            public void toDto(Book source, BookDTO destination) {
+                destination.setName(source.getName());
+                destination.setLanguage(source.getLanguage());
+                destination.setYearOfPublishing(source.getYearOfPublishing());
+                AuthorDTO authorDTO = new AuthorDTO();
+                authorConverter.toDto(source.getAuthor(), authorDTO);
+                destination.setAuthor(authorDTO);
+                Set<GenreDTO> genres = new HashSet<>();
+                genreConverter.toDto(source.getGenres(), genres);
+                destination.setGenres(genres);
+            }
+
+            public void fromDTO(BookDTO source, Book book) {
+                book.setId(source.getId());
+                book.setName(source.getName());
+                book.setLanguage(source.getLanguage());
+                book.setYearOfPublishing(source.getYearOfPublishing());
+                book.setGenres(source.getGenres()
+                        .stream()
+                        .map(genreConverter::fromDTO)
+                        .collect(Collectors.toList()));
+                Author author = new Author();
+                authorConverter.fromDTO(source.getAuthor(), author);
+                book.setAuthor(author);
+            }
+        }
+        ```
+    """.trimIndent()
 }
